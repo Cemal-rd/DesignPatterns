6ceeb19ea8242df6e65e347e8ae43c75db00f89d
Cemal Ordu <rdcemal@gmail.com>
2024-03-10 03:53:15 +0300

    Add project files.

---
+++ BuilderPattern/Program.cs
@@ -0,0 +1,11 @@
+﻿using BuilderPattern.Method1;
+using System.Text;
+
+var sb = new StringBuilder();
+sb.Append("Cemal");
+sb.Append("ordu");
+var fullname=sb.ToString();
+var eb = new EndpointBuilder("https://localhost");
+eb.Append("api").Append("v1").Append("user").AppendParam("id", "5");
+var url = eb.Build();
+System.Console.WriteLine(url);
