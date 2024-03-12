8515339d8ca8443c456227e8d4dddf25089e60ac
Cemal Ordu <rdcemal@gmail.com>
2024-03-12 03:26:45 +0300

    Singleton Pattern

---
+++ Singleton Pattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using Singleton_Pattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Singleton nesneye erişim.
+        Singleton singleton1 = Singleton.Instance;
+        Singleton singleton2 = Singleton.Instance;
+
+        // İki nesne aynı nesne mi kontrolü.
+        if (singleton1 == singleton2)
+        {
+            Console.WriteLine("Aynı singleton nesnesine erişildi.");
+        }
+        else
+        {
+            Console.WriteLine("Farklı singleton nesnelerine erişildi.");
+        }
+
+        // Singleton sınıfından bir işlem gerçekleştirilir.
+        singleton1.DoSomething();
+
+        Console.ReadLine();
+    }
+}
---
+++ Singleton Pattern/Singleton.cs
@@ -0,0 +1,42 @@
+﻿using Singleton_Pattern.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Pattern
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance = null;
+        private static readonly object padlock = new object();
+
+        // Constructor private olarak tanımlanır, böylece dışarıdan erişim engellenir.
+        private Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                // Birden fazla thread'in bu bloğa girmesini engellemek için lock kullanılır.
+                lock (padlock)
+                {
+                    // Eğer henüz bir örnek oluşturulmamışsa, yeni bir örnek oluşturulur.
+                    if (instance == null)
+                    {
+                        instance = new Singleton();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Singleton sınıfından bir işlem gerçekleştirildi.");
+        }
+    }
+}
