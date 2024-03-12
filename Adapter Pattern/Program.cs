aeb78852ec20b7ad7a0d7b1ab77a37f8baf1a8e0
Cemal Ordu <rdcemal@gmail.com>
2024-03-12 03:50:16 +0300

    Adapter Pattern

---
+++ Adapter Pattern/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+// Hedef Arayüz
+interface IMediaPlayer
+{
+    void Play(string fileName);
+}
+
+// Adapte Edilen Sınıf - MP3 Çalıcı
+class Mp3Player
+{
+    public void PlayMp3(string fileName)
+    {
+        Console.WriteLine("MP3 dosyası çalınıyor: " + fileName);
+    }
+}
+
+// Adaptör
+class Mp3ToMediaPlayerAdapter : IMediaPlayer
+{
+    private Mp3Player mp3Player;
+
+    public Mp3ToMediaPlayerAdapter(Mp3Player mp3Player)
+    {
+        this.mp3Player = mp3Player;
+    }
+
+    public void Play(string fileName)
+    {
+        mp3Player.PlayMp3(fileName);
+    }
+}
+
+// İstemci
+class Client
+{
+    public void Run(IMediaPlayer mediaPlayer, string fileName)
+    {
+        mediaPlayer.Play(fileName);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Adapte edilen sınıf örneği
+        Mp3Player mp3Player = new Mp3Player();
+
+        // Adaptör aracılığıyla Mp3Player sınıfının metodu IMediaPlayer arayüzüne dönüştürülüyor
+        IMediaPlayer adapter = new Mp3ToMediaPlayerAdapter(mp3Player);
+
+        // İstemci, hedef arayüz üzerinden işlem yapar
+        Client client = new Client();
+        client.Run(adapter, "song.mp3");
+
+        Console.ReadLine();
+    }
+}
--- Singleton Pattern/Singleton.cs
+++ Singleton Pattern/Singleton.cs
@@ -1,4 +1,4 @@
-﻿using Singleton_Pattern.Models;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
